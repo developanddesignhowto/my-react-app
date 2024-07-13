04a36d6b90a16cd996838bc01ad8f2930648c203
Develop and Design <johnjmac49@gmail.com>
2024-07-13 11:04:46 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Blog from './components/Blog';
+
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+
           </ul>
         </nav>
 
@@ -19,6 +23,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+
         </Routes>
       </div>
     </Router>
